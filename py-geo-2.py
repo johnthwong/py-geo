877020a9678cb0f3aab1162c480d9cc8cfb0348b
john <wongthjohn@gmail.com>
2023-04-26 21:45:12 +0800

    .

--- py-geo-2.py
+++ py-geo-2.py
@@ -7,40 +7,42 @@ InteractiveShell.ast_node_interactivity = "all"
 import geopandas
 # import shapely
 # import pysal 
-#  the pysal metapackage doesnt actually contain any code, just a set of dependencies on its constituent subpackages. 
+#  the pysal metapackage doesnt actually contain any code, 
+    # just a set of dependencies on its constituent subpackages. 
 # Chances are good you don't actually need everything in the metapackage, 
 # so you could also install the subpackages you are looking to use directly
 # import fiona
 # import pyproj
 import pyrosm
 # import osmnx
-import matplotlib
+# import matplotlib
 import os
-# pip install folium matplotlib mapclassify
+# conda install -c conda-forge folium matplotlib mapclassify
+from dash import Dash, dcc, html
+import dash_leaflet
+import json
+
 
 os.getcwd()
 # os.chdir("C:/Users/John TH Wong/Documents")
 # os.chdir("/Users/john/Library/Mobile Documents/com~Apple~CloudDocs/research/coding/py-geo")
-wd = print(os.getcwd())
-
+def wd(): 
+    return os.getcwd()
+wd = wd()
 
 # %% 
 # CSDI PORTAL DATA ----
 
-fp_a = "data/POS.json" # Public Open Space
-df_a = geopandas.read_file(fp_a)
-
-fp_b = "data/DCD.json" # District Council Boundaries
-
-df_b = geopandas.read_file(fp_b)
+df_openspace = geopandas.read_file("data/POS.json") # Public Open Space
+df_dist_boundaries = geopandas.read_file("data/DCD.json") # District Council Boundaries
 
 # %%
-df_a.head()
-df_b.head()
+df_openspace.head()
+df_dist_boundaries.head()
 #%%
-df_a = df_a.drop(columns = 'LASTUPDATE')
-df_a.explore()
-df_b.explore()
+df_openspace = df_a.drop(columns = 'LASTUPDATE')
+df_openspace.explore()
+df_dist_boundaries.explore()
 
 # %%
 # OPENSTREETMAP DATA ----
@@ -94,9 +96,12 @@ restaurants = osm.get_pois(custom_filter = {'amenity': ['restaurant']} )
 restaurants.plot()
 
 # %%
-# this merges buildings to observations of restautrants, where the two intersect
-# the predicate argument, unused below, defaults to intersects; others include contains, crosses, etc.
-# .sjoin_nearest() joins based on "closest", with max distance set by user, rather than "touching"
+# this merges buildings to observations of restautrants, 
+    # where the two intersect
+# the predicate argument, unused below, defaults to intersects; 
+    # others include contains, crosses, etc.
+# .sjoin_nearest() joins based on "closest", 
+    # with max distance set by user, rather than "touching"
 # overlay() keeps both rather than merges
 # .sjoin can be called from both geopandas package and GeoDataFrames objects
 join = geopandas.sjoin(buildings, restaurants, how = 'inner')
@@ -108,14 +113,18 @@ buildings.describe()
 buildings
 buildings.columns
 type(buildings['geometry'])
-# overlay = geopandas.overlay(restaurants, buildings, how = 'intersection', keep_geom_type = False)
+# overlay = geopandas.overlay(restaurants, buildings, 
+#                             how = 'intersection', keep_geom_type = False)
 
 # %%
 roads = osm.get_network()
 # %%
-plot = join.plot(column = 'building_left', figsize = (12,12), cmap = 'RdYlBu', legend = True)
+plot = join.plot(column = 'building_left', figsize = (12,12), cmap = 'RdYlBu', 
+                 legend = True)
 plot = roads.plot(edgecolor = 'gray', linewidth = 0.75,
-                ax = plot # this argument aligns the roads.plot with the plot object from previous line
+                 # this argument aligns the roads.plot with the plot object 
+                     # from previous line
+                ax = plot 
                 )
 plot.set_ylim([52.36, 52.38])
 plot.set_xlim([4.88, 4.91])
@@ -124,36 +133,90 @@ plot.set_xlim([4.88, 4.91])
 # %%
 # CSDI PORTAL DATA 2 ----
 
-hk_buildings = geopandas.read_file('data/building_FSDT/BUILDING_STRUCTURE.json')
+df_hk_buildings = geopandas.read_file('data/building_FSDT/BUILDING_STRUCTURE.json')
 # %%
-hk_buildings.plot()
-# hk_buildings.explore().save('map.html')
+df_hk_buildings.plot()
 # %%
-# hk_buildings.describe()
-hk_buildings.columns
-hk_buildings['geometry']
+# df_hk_buildings.describe()
+df_hk_buildings.columns
+# df_hk_buildings['geometry']
 # %%
-hk_roads = geopandas.read_file('data/roads_FSDT/CENTERLINE.json')
+df_hk_roads = geopandas.read_file('data/roads_FSDT/CENTERLINE.json')
 # %%
-# hk_buildings.explore()
+df_hk_slopes = geopandas.read_file('data/SMR_FSDT/SMR_BDY_POLY.json')
+# df_hk_govland = geopandas.read_file('data/govland_FSDT/GovernmentLandAllocation.json')
 # %%
-# hk_slopes = geopandas.read_file('data/SMR_FSDT/SMR_BDY_POLY.json')
-# hk_govland = geopandas.read_file('data/govland_FSDT/GovernmentLandAllocation.json')
+df_bldg_pos = geopandas.sjoin(df_hk_buildings, df_openspace, how = 'inner')
 # %%
-join_bldg_pos = geopandas.sjoin(hk_buildings, df_a, how = 'inner')
-# %%
-hk_buildings['CATEGORY']
-plot_bldg_pos = join_bldg_pos.plot(figsize = (12,12),
+df_hk_buildings['CATEGORY']
+plot_bldg_pos = df_bldg_pos.plot(figsize = (12,12),
                                    cmap = 'RdYlBu',
                                    column = 'CATEGORY',
                                    legend = True)
-plot_bldg_pos = hk_roads.plot(edgecolor = 'gray', linewidth = 0.75, ax = plot_bldg_pos)
+plot_bldg_pos = df_hk_roads.plot(edgecolor = 'gray', linewidth = 0.75, 
+                                 ax = plot_bldg_pos)
 plot_bldg_pos.set_ylim([815000, 822500])
 plot_bldg_pos.set_xlim([832500, 837500])
 
 
 # %%
-join_bldg_pos = join_bldg_pos.drop(columns = ['STATUSDATE', 'LASTUPDATE'])
-join_bldg_pos.explore().save('map.html')
+df_bldg_pos = df_bldg_pos.drop(columns = ['STATUSDATE', 'LASTUPDATE'])
+# join_bldg_pos.explore().save('map.html')
+
+#%%
+df_bldg_slopes = geopandas.sjoin(df_hk_buildings, df_hk_slopes, how = 'inner')
+plot_bldg_slopes = df_bldg_slopes.plot(figsize = (12,12),
+                                   cmap = 'RdYlBu',
+                                   column = 'CATEGORY',
+                                   legend = True)
+plot_bldg_slopes = df_hk_roads.plot(edgecolor = 'gray', linewidth = 0.75,
+                                    ax = plot_bldg_slopes)
+plot_bldg_slopes.set_xlim([830000,845000])
+plot_bldg_slopes.set_ylim([810000,825000])
+
+#%%
+df_map = geopandas.read_file('data/map_topographic_50000/ELEVLINE.json')
+plot_map = df_map.plot()
+plot_map.set_xlim([830000,845000])
+plot_map.set_ylim([810000,825000])
+
+
+
+#%%
+df_bldg_slopes_json = df_bldg_slopes.to_json()
+
+app = Dash()
+
+app.layout = dash_leaflet.Map(
+    [dash_leaflet.TileLayer(),
+     dash_leaflet.GeoJSON(data=df_bldg_slopes_json,
+                          zoomToBounds=True, zoomToBoundsOnClick=True)    
+     ],
+    style = {'width': '10-0px', 'height': '500px'}
+    )
+
+
+
+#%%
+if __name__ == '__main__':
+    app.run_server()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
